282323b37d6156ea5a252abf810d18059195236d
mkmkmkmk11 <vkxl0512@naver.com>
2021-08-23 23:24:31 +0900

    controller 수정 list 수정

--- Final_Project_05/src/main/java/com/minton/minton05/cafe/controller/CafeController.java
+++ Final_Project_05/src/main/java/com/minton/minton05/cafe/controller/CafeController.java
@@ -1,11 +1,20 @@
 package com.minton.minton05.cafe.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.minton.minton05.cafe.dto.CafeCommentDto;
+import com.minton.minton05.cafe.dto.CafeDto;
 import com.minton.minton05.cafe.service.CafeService;
 
 
@@ -20,6 +29,88 @@ public class CafeController {
 		service.getList(request);
 		
 		return "cafe/list";
+	}
+	
+	@RequestMapping("/cafe/private/insertform")
+	public String insertForm() {
+		
+		return "cafe/insertform";
+	}
+	//새글 저장 요청 처리 
+	@RequestMapping("/cafe/private/insert")
+	public String insert(CafeDto dto, HttpSession session) {
+		//글 작성자는 세션에서 얻어낸다. 
+		String id=(String)session.getAttribute("id");
+		//CafeDto 객체에 글 작성자도 담기
+		dto.setWriter(id);
+		service.saveContent(dto);
+		
+		return "cafe/insert";
+	}
+	//글 자세히 보기 요청 처리
+	@RequestMapping("/cafe/detail")
+	public String detail(HttpServletRequest request) {
+		service.getDetail(request);
+		return "cafe/detail";
+	}
+	//새로운 댓글 저장 요청 처리
+	@RequestMapping("/cafe/private/comment_insert")
+	public String commentInsert(HttpServletRequest request, 
+			@RequestParam int ref_group) {
+		
+		service.saveComment(request);
+		
+		return "redirect:/cafe/detail.do?num="+ref_group;
+	}
+	//댓글 더보기 요청 처리
+	@RequestMapping("/cafe/ajax_comment_list")
+	public String ajaxCommentList(HttpServletRequest request) {
+		
+		service.moreCommentList(request);
+		
+		return "cafe/ajax_comment_list";
+	}
+    //댓글 삭제 요청 처리
+    @RequestMapping("/cafe/private/comment_delete")
+    @ResponseBody
+    public Map<String, Object> commentDelete(HttpServletRequest request) {
+	      service.deleteComment(request);
+	      Map<String, Object> map=new HashMap<String, Object>();
+	      map.put("isSuccess", true);
+	      // {"isSuccess":true} 형식의 JSON 문자열이 응답되도록 한다. 
+	      return map;
+    }
+	//댓글 수정 요청처리 (JSON 을 응답하도록 한다)
+	@RequestMapping("/cafe/private/comment_update")
+	@ResponseBody
+	public Map<String, Object> commentUpdate(CafeCommentDto dto){
+	      service.updateComment(dto);
+	      Map<String, Object> map=new HashMap<String, Object>();
+	      map.put("isSuccess", true);
+	      // {"isSuccess":true} 형식의 JSON 문자열이 응답되도록 한다. 
+	      return map;
+	}
+	//카페글 삭제 요청 처리
+	@RequestMapping("/cafe/private/delete")
+	public String delete(@RequestParam int num, HttpServletRequest request) {
+		
+		service.deleteContent(num, request);
+		
+		return "redirect:/cafe/list.do";
+	}
+	
+	@RequestMapping("/cafe/private/updateform")
+	public String updateForm(HttpServletRequest request) {
+		
+		service.getData(request);
+		
+		return "cafe/updateform";
+	}
+	//카페글 수정 요청 처리
+	@RequestMapping(value= "/cafe/private/update", method = RequestMethod.POST)
+	public String update(CafeDto dto) {
+		service.updateContent(dto);
+		return "cafe/update";
 	}
 }
 
--- Final_Project_05/src/main/webapp/WEB-INF/views/cafe/list.jsp
+++ Final_Project_05/src/main/webapp/WEB-INF/views/cafe/list.jsp
@@ -105,21 +105,21 @@
 	               <%-- 댓글 개수 출력 --%>
 					<span class="mx-2" style="color:#198754;"><%--CafeCommentDao.getInstance().getCount(tmp.getNum())--%></span>
 	            	<%-- 이미지가 첨부되어 있을 시 아이콘 출력 --%>
-	            	<%--CafeDto dto2=CafeDao.getInstance().getData(tmp.getNum());			
-					<c:if test="${img not empty }">
+	            	<%--CafeDto dto2=CafeDao.getInstance().getData(tmp.getNum());
+	            	if(dto2.getContent().contains("img")){			
+					<c:if test="${dto.content == img }">
 							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
 								<path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
 								<path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
 							</svg>
-						</a>
 					</c:if>	
 					--%>
 	            </td>
 	         </tr>		
 	  </c:forEach>
       
-            <td>
             <%-- 작성자 왼쪽에 프로필 사진 출력 
+            <td>
             <%
             UsersDto usersDto = UsersDao.getInstance().getData(tmp.getWriter());
             if(usersDto.getProfile()!=null){
