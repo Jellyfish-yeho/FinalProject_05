fc984a97e93185964e82dea5a0cf17e09e78f50a
mkmkmkmk11 <vkxl0512@naver.com>
2021-08-26 15:23:13 +0900

    NoticeSevice와 SeviceImpl 수정

--- Final_Project_05/src/main/java/com/minton/minton05/notice/service/NoticeServiceImpl.java
+++ Final_Project_05/src/main/java/com/minton/minton05/notice/service/NoticeServiceImpl.java
@@ -184,6 +184,70 @@ public class NoticeServiceImpl implements NoticeService{
 		//exception 작업 예정
 		noticeDao.delete(num);	
 	}
+
+	@Override
+	public List<NoticeDto> getList2(HttpServletRequest request) {
+		//한 페이지에 몇개씩 표시할 것인지
+		final int PAGE_ROW_COUNT=10;
+		//하단 페이지를 몇개씩 표시할 것인지
+		final int PAGE_DISPLAY_COUNT=5;
+		   
+		//보여줄 페이지의 번호를 일단 1이라고 초기값 지정
+		int pageNum=1;
+		//페이지 번호가 파라미터로 전달되는지 읽어와 본다.
+		String strPageNum=request.getParameter("pageNum");
+		//만일 페이지 번호가 파라미터로 넘어 온다면
+		if(strPageNum != null){
+			//숫자로 바꿔서 보여줄 페이지 번호로 지정한다.
+			pageNum=Integer.parseInt(strPageNum);
+		}
+		   
+		//보여줄 페이지의 시작 ROWNUM
+		int startRowNum=1+(pageNum-1)*PAGE_ROW_COUNT;
+		//보여줄 페이지의 끝 ROWNUM
+		int endRowNum=pageNum*PAGE_ROW_COUNT;
+		
+	      /*
+        [ 검색 키워드에 관련된 처리 ]
+        -검색 키워드가 파라미터로 넘어올수도 있고 안넘어 올수도 있다.      
+	     */
+	     String keyword=request.getParameter("keyword");
+	     String condition=request.getParameter("condition");
+	     //만일 키워드가 넘어오지 않는다면 
+	     if(keyword==null){
+	        //키워드와 검색 조건에 빈 문자열을 넣어준다. 
+	        //클라이언트 웹브라우저에 출력할때 "null" 을 출력되지 않게 하기 위해서  
+	        keyword="";
+	        condition=""; 
+	     }
+	     //특수기호를 인코딩한 키워드를 미리 준비한다. 
+	     String encodedK=URLEncoder.encode(keyword);
+	         
+	     //Dto 객체에 startRowNum 과 endRowNum 을 담는다.
+	     NoticeDto dto=new NoticeDto();
+	     dto.setStartRowNum(startRowNum);
+	     dto.setEndRowNum(endRowNum);
+
+	      //만일 검색 키워드가 넘어온다면 
+	      if(!keyword.equals("")){
+	    	  //검색 조건이 무엇이냐에 따라 분기 하기
+	    	  if(condition.equals("title_content")){//제목 + 내용 검색인 경우
+	    		  //검색 키워드를 NoticeDto 에 담아서 전달한다.
+	    		  dto.setTitle(keyword);
+	    		  dto.setContent(keyword);
+	    	  }else if(condition.equals("title")){ //제목 검색인 경우
+	    		  dto.setTitle(keyword);
+	    	  }else if(condition.equals("writer")){ //작성자 검색인 경우
+	    		  dto.setWriter(keyword);
+	    	  } // 다른 검색 조건을 추가 하고 싶다면 아래에 else if() 를 계속 추가 하면 된다.
+	      }
+	      //글 목록 얻어오기 
+	      List<NoticeDto> list=noticeDao.getList(dto);
+	      //전체글의 갯수
+	      int totalRow=noticeDao.getCount(dto);
+	      
+	      return list;
+	}
 	
 }
 
